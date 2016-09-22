ea19f7de67b24199d6e9626a45b6b6e8c5a470c3
moniquedesalvo <moniquerenee@gmail.com>
2016-09-22 14:08:03 -0700

    details section added

--- public/mockup.jsx
+++ public/mockup.jsx
@@ -61,6 +61,12 @@ window.packing = [
 	} 
 ];
 
+window.details = [
+	{
+	text: "Muffin apple pie bear claw muffin croissant sweet donut pie. Cotton candy soufflé caramels icing cake puddingdonut. Halvah biscuit toffee pudding tiramisu. Icing cookie liquorice macaroon gummies candy caramels sugar plum croissant. Danish gummies caramels danish topping candy jelly-o gummi bears. Lemon drops cake candy cake. Caramels gingerbread topping. Candy tiramisu dessert sesame snaps. Sugar plum chupa chups powder donut caramels. Muffin candy icing jelly beans. Powder fruitcake cake carrot cake marshmallow lollipop cake tiramisu. Tootsie roll cake apple pie.\n\nGummies jelly-o cheesecake. Muffin dragée cake croissant dessert wafer cheesecake caramels carrot cake. Marzipan gummi bears liquorice carrot cake marshmallow apple pie chupa chups gummies. Wafer cookie jelly tiramisu wafer cake jelly beans cookie. Brownie macaroon bear claw. Apple pie oat cake cookie biscuit croissant jelly beans lollipop. Candy icing sugar plum cupcake halvah fruitcake. Sesame snaps cake icing sweet gummies. Croissant tart bear claw. Sesame snaps cupcake croissant gummi bears gummies apple pie. Oat cake toffee cake halvah gingerbread muffin. Lemon drops danish toffee chupa chups sesame snaps carrot cake chocolate cake oat cake cupcake. Marzipan jelly beans candy candy canes.\n\nSesame snaps brownie sweet cheesecake topping. Bear claw cookie jelly beans jelly-o bonbon liquorice caramels. Bear claw muffin bonbon toffee chocolate jelly chupa chups brownie marzipan. Pastry oat cake dragée powder marshmallow lollipop. Marzipan jelly beans macaroon. Cookie tootsie roll dragée dessert dessert apple pie danish cupcake. Bonbon macaroon sweet candy canes gingerbread liquorice marshmallow bonbon marzipan. Dragée gingerbread jelly lemon drops jelly cake toffee. Gummi bears cake tiramisu brownie. Chocolate halvah pie caramels carrot cake. Candy chocolate bar lemon drops. Bonbon powder brownie. Oat cake oat cake cheesecake dessert cake.\n\nDragée chupa chups dessert gummi bears pudding carrot cake oat cake. Pie sugar plum lollipop lemon drops macaroon. Pastry oat cake caramels halvah. Topping jelly beans toffee caramels jujubes danish wafer. Candy toffee candy oat cake gummies toffee sugar plum. Toffee powder sugar plum macaroon chupa chups brownie cotton candy pudding sweet roll. Biscuit carrot cake bear claw sweet sweet roll biscuit liquorice. Carrot cake dessert chocolate cake oat cake. Topping chocolate tart ice cream powder oat cake. Tart halvah toffee tootsie roll carrot cake cookie oat cake cake. Tart macaroon caramels brownie tootsie roll. Fruitcake tiramisu ice cream. Chocolate soufflé sugar plum gummies gingerbread. Jelly beans liquorice caramels fruitcake ice cream fruitcake sugar plum apple pie."
+	}
+];
+
 
 var TitleSection = React.createClass({
 	render: function () {
@@ -68,7 +74,6 @@ var TitleSection = React.createClass({
 			<div className="title-section">
 				<h1>{title}</h1>
 			</div>
-
 		);
 	}
 })
@@ -208,7 +213,6 @@ var VoteSection = React.createClass({
 	},
 	addVote: function () {
 		var i = this.props.index;
-		console.log('add vote called')
 		if (this.state.hasVoted === false) {
 			this.setState({hasVoted: true})
 			choices[i].likes += 1;
@@ -217,7 +221,6 @@ var VoteSection = React.createClass({
 	},
 	removeVote: function () {
 		var i = this.props.index;
-		console.log('remove vote called')
 		if (this.state.hasVoted === true) {
 			this.setState({hasVoted: false})
 			choices[i].likes -= 1;
@@ -486,6 +489,55 @@ var PackingRow = React.createClass({
 	}
 });
 
+var Details = React.createClass({
+	getInitialState: function () {
+		return {
+			isEditing: false
+		}
+	},
+	render: function () {
+		if (this.state.isEditing === true) {
+			var view =
+			<div id="details-section">
+				<h2>Details:</h2>
+				<textarea defaultValue={details[0].text} type="text" size="50" ref="detailsInput"></textarea>
+				<button onClick={this.handleClick}>Submit</button>
+			</div>
+		} else {	
+			view = 
+			<div id="details-section">
+				<h2>Details:</h2>
+				<p>{details[0].text}</p>
+				<button onClick={this.toggleEditOn}>Edit</button>
+			</div>
+		}
+		return (
+			view
+		)
+	},
+	saveDetails: function () {
+		var detailsInput = this.refs.detailsInput;
+		var detailsText = detailsInput.value;
+		details[0].text = detailsText;
+		console.log(details[0].text)
+		renderApp();
+	},
+	toggleEditOn: function () {
+		if (!this.state.isEditing) {
+			this.setState({isEditing: true})
+		}
+	},
+	toggleEditOff: function () {
+		if (this.state.isEditing) {
+			this.setState({isEditing: false})
+		}
+	},
+	handleClick: function () {
+		this.saveDetails();
+		this.toggleEditOff();
+	}
+})
+
 var App = React.createClass({
 	render: function () {
 		return (
@@ -495,15 +547,14 @@ var App = React.createClass({
 					{this.findBooked()}
 					<AttendeesSection />
 					<PackingSection />
+					<Details />
 				</div>
 			</div>
 		)
 	},
 	findBooked: function () {
 		for (var i = 0; i < choices.length; i++) {
-			console.log(choices[i])
 			if (choices[i].booked === true) {
-				console.log("booked, got here")
 				return <Booked index={i}/>
 			}
 		}
